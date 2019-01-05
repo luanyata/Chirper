4c84936fa7d11273403ea17c7e76f9ab4dedb9ee
Luan Lima <luanyata@gmail.com>
2019-01-05 18:20:16 -0200

    Fechamento do modulo de react redux no mundo real

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleAddTweet } from '../actions/tweets'
+import { Redirect } from 'react-router-dom'
 
 class NewTweet extends Component {
     state = {
-        text: ''
+        text: '',
+        toHome: false,
     }
 
     handleChange = (e) => {
@@ -25,15 +27,19 @@ class NewTweet extends Component {
         dispatch(handleAddTweet(text, id))
 
         this.setState(() => ({
-            text: ''
+            text: '',
+            toHome: id ? false : true
         }))
+
     }
 
     render() {
 
-        const { text } = this.state
+        const { text, toHome } = this.state
 
-        const tweetLeft = 200 - text.length
+        if (toHome) return <Redirect to='/' />
+
+        const tweetLeft = 280 - text.length
 
         return (
             <div>
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -6,6 +6,7 @@ import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
 
@@ -23,6 +24,7 @@ class Tweet extends Component {
 
     toParent = (e, id) => {
         e.preventDefault();
+        this.props.history.push(`/tweet/${id}`)
     }
 
     render() {
@@ -31,10 +33,10 @@ class Tweet extends Component {
 
         if (tweet === null) return <p>This Tweet doesnt existed</p>
 
-        const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet;
+        const { name, avatar, timestamp, text, hasLiked, likes, replies, id, parent } = tweet;
 
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img
                     src={avatar}
                     alt={`Avatar of ${name}`}
@@ -62,7 +64,7 @@ class Tweet extends Component {
                         <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -79,4 +81,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
